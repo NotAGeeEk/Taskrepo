4e56f45be295c6b90b0fce6afefda49a87eac5d3
Amans <notshashank10@gmail.com>
2023-09-15 07:24:16 +0530

    changes accepted

---
+++ Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using WebAppUsers.Models;
+
+namespace WebAppUsers.Controllers
+{
+    public class UserController : Controller
+    {
+        static List<User> users = new List<User>
+        {
+            new User(){userId=1,Username="sam",UserPwd="sam@1256",UserEmail="sam@yahoo.com"},
+            new User(){userId=2,Username="amit",UserPwd="amit@123",UserEmail="amit@hotmail.com"},
+            new User(){userId=3,Username="kumar",UserPwd="kumar@1256",UserEmail="kumar@mail.com"},
+            new User(){userId=4,Username="anisha",UserPwd="a@rohan",UserEmail="a@yahoomail.com"},
+            new User(){userId=5,Username="vidisha",UserPwd="v@isha",UserEmail="v5@gmail.com"}
+
+
+        };
+        public IActionResult Index()
+        {
+            return View(users);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new User());
+        }
+        [HttpPost]
+        public IActionResult Create(User Model)
+        {
+            if (ModelState.IsValid)
+            {
+                users.Add(Model);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(Model);
+            }
+        }
+        
+
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,19 @@
-﻿namespace WebAppUsers.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppUsers.Models
 {
     public class User
     {
+        [Required]
+        public int userId { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string UserPwd { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string UserEmail { get; set; }
+
     }
 }
